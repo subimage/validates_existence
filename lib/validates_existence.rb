aeae9c66d9231dd9435ba9a1753b5787c35be967
Seth Banks <stb@subimage.com>
2012-09-24 20:51:08 -0700

    Fix error that can be thrown on constantize.
    If you have API clients sending data, they can send arbitrary strings
    that get passed to constantize.
    
    This of course, causes errors.
    
    Fixed.

--- lib/validates_existence.rb
+++ lib/validates_existence.rb
@@ -1,63 +1,73 @@
-module ActiveRecord
-  module Validations
+module ActiveRecord::Validations
 
-    def self.included(base)
-      base.extend ClassMethods
-    end
+  def self.included(base)
+    base.extend ClassMethods
+  end
 
-    module ClassMethods
+  module ClassMethods
 
-      # The validates_existence_of validator checks that a foreign key in a belongs_to
-      # association points to an exisiting record. If :allow_nil => true, then the key
-      # itself may be nil. A non-nil key requires that the foreign object must exist.
-      # Works with polymorphic belongs_to.
-      def validates_existence_of(*attr_names)
-        configuration = { :message => "does not exist", :on => :save }
-        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
+    # The validates_existence_of validator checks that a foreign key in a belongs_to
+    # association points to an exisiting record. If :allow_nil => true, then the key
+    # itself may be blank/nil. A non-blank key requires that the foreign object must exist.
+    # Works with polymorphic belongs_to.
+    def validates_existence_of(*attr_names)
+      configuration = { :message => "does not exist", :on => :save }
+      configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
 
-        attr_names.each do |attr_name|
-          unless (assoc = reflect_on_association(attr_name)) && assoc.macro == :belongs_to
-            raise ArgumentError, "Cannot validate existence of :#{attr_name} because it is not a belongs_to association."
-          end
-          send(validation_method(configuration[:on])) do |record|
-            unless configuration[:if] && !evaluate_condition(configuration[:if], record)
-              fk_value = record[assoc.primary_key_name]
-              unless fk_value.nil? && configuration[:allow_nil]
-                if (foreign_type = assoc.options[:foreign_type]) # polymorphic
-                  foreign_type_value = record[assoc.options[:foreign_type]]
-                  if !foreign_type_value.blank?
-                    assoc_class = foreign_type_value.constantize
-                  else
-                    record.errors.add(attr_name, configuration[:message])
-                    next
-                  end
-                else # not polymorphic
-                  assoc_class = assoc.klass
+      attr_names.each do |attr_name|
+        unless (assoc = reflect_on_association(attr_name)) && assoc.macro == :belongs_to
+          raise ArgumentError, "Cannot validate existence of :#{attr_name} because it is not a belongs_to association."
+        end
+        send(validation_method(configuration[:on])) do |record|
+          unless configuration[:if] && !evaluate_condition(configuration[:if], record)
+            
+            # Allow using associations that don't use ID as a foreign key
+            assoc_key = (assoc.options[:primary_key] || :id)
+
+            fk_value = record[assoc.primary_key_name]
+            next if fk_value.blank? && configuration[:allow_nil]
+              
+            if (foreign_type = assoc.options[:foreign_type]) # polymorphic
+              foreign_type_value = record[assoc.options[:foreign_type]]
+              
+              assoc_class = nil
+              
+              if !foreign_type_value.blank?
+                begin
+                  assoc_class = foreign_type_value.constantize
+                rescue
                 end
-                
-                # Allow checking to see if associated item has same field value
-                if scope = configuration[:scope]
-                  Array(scope).map do |scope_item|
-                    scope_value = record.send(scope_item)
-                    unless assoc_class && assoc_class.find(:first, :conditions => [
-                      "id = ? AND #{scope_item} = ?", fk_value, scope_value
-                    ])
-                      record.errors.add(attr_name, configuration[:message]) 
-                    end
-                  end
-                else
-                  unless assoc_class && assoc_class.exists?(fk_value)
-                    record.errors.add(attr_name, configuration[:message]) 
-                  end
+              else
+                record.errors.add(attr_name, configuration[:message])
+                next
+              end
+            else # not polymorphic
+              assoc_class = assoc.klass
+            end
+            
+            # Allow checking to see if associated item has same field value
+            if scope = configuration[:scope]
+              Array(scope).map do |scope_item|
+                scope_value = record.send(scope_item)
+                unless assoc_class && assoc_class.find(:first, :conditions => [
+                  "#{assoc_key} = ? AND #{scope_item} = ?", fk_value, scope_value
+                ])
+                  record.errors.add(attr_name, configuration[:message]) 
                 end
-                
+              end
+            else
+              unless assoc_class && assoc_class.exists?(
+                ["#{assoc_key} = ?", fk_value]
+              )
+                record.errors.add(attr_name, configuration[:message]) 
               end
             end
+              
           end
         end
-      end
+      end # end attr_names
+    end
 
-    end # ClassMethods
+  end # ClassMethods
 
-  end
 end
